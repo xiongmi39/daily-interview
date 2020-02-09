a96b691b814a38e48abb7024d17ea88f4d81222a
xiongmi39 <xiongmi39@163.com>
2020-02-09 11:03:03 +0800

    数组扁平化

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import "babel-polyfill";
 // import Utils from './js/utils/utils.js';
 
 import test from './js/test.js';
+import getFlatArr from './js/programing/flatArr1.js';
 
  // let utils = new Utils();
 
--- js/programing/flatArr1.js
+++ js/programing/flatArr1.js
@@ -7,7 +7,20 @@
 
 //     编写一个程序将数组扁平化去并除其中重复部分数据，最终得到一个升序且不重复的数组
 
+//解法1
+// var getFlatArr = function(arr){
+// 	return [...new Set(arr.toString().split(',').sort((a,b)=>a-b))].map(Number);
+// };
+
+//解法2
+Array.prototype.flat = function(){
+	return [].concat(...this.map((item)=> (Array.isArray(item) ? item.flat(): [item])));
+}
+Array.prototype.unique = function(){
+	return [...new Set(this)];
+}
 var getFlatArr = function(arr){
+	return arr.flat().unique().sort((a,b)=> a-b);
 
 };
 
